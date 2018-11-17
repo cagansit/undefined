176d6df099a50788614ca58a208a9974bc7b5a88
onurssx9 <onurssx9@gmail.com>
2018-11-17 14:51:52 +0300

    Search

--- extension/app/components/Header.js
+++ extension/app/components/Header.js
@@ -1,16 +1,15 @@
 import React, { PropTypes, Component } from 'react';
 
 import style from './Header.css';
+import Search from './Search';
 
 export default class Header extends Component {
   static propTypes = {};
 
-  search = (text) => {};
-
   render() {
     return (
       <header className={style.header}>
-        <h1>todos</h1>
+        <Search />
       </header>
     );
   }
--- extension/app/reducers/todos.js
+++ extension/app/reducers/todos.js
@@ -1,16 +1,16 @@
 import { fromJS } from 'immutable';
-import {
-
-} from '../constants/ActionTypes';
+import { SEARCH_FILTER } from '../constants/ActionTypes';
 
 const initialState = fromJS({
   isCreationMode: false,
+  searhFilter: ''
 });
-
 
 /* eslint-disable new-cap */
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case SEARCH_FILTER:
+      return state.set('searhFilter', action.text);
     default:
       return state;
   }
