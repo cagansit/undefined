e1bad31e1ba1a38364ed0f1d2fbea48653cfd07e
muhammet-insider <muhammet@useinsider.com>
2018-11-17 23:31:10 +0300

    code mirror integration

--- extension/app/components/Create.js
+++ extension/app/components/Create.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import CodeMirror from 'react-codemirror';
+import style from './Create.css';
+
+const CodeMirrorOptions = {
+  lineNumbers: true,
+  mode: 'javascript',
+};
 
 export default class Create extends Component {
+  state = {
+    code: '',
+  }
+
+  updateCode = (newValue) => {
+    this.setState({ code: newValue });
+  }
+
   render() {
     return (
-      <div>
+      <div className={style.container} >
         Create Page
+        <CodeMirror value={this.state.code} onChange={this.updateCode} options={CodeMirrorOptions} />
       </div>
     );
   }
